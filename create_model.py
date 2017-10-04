224c98c42b32e8edd99167879f733fede2012960
Drew McArthur <drewmcarthur1@gmail.com>
2017-10-04 10:26:09 +0200

    finalized reading file, and first prepocessing object

--- create_model.py
+++ create_model.py
@@ -1,9 +1,26 @@
 """ defines functions to train a model given a dataset. """
 
+from sklearn.neuralnetwork import MLPClassifier
+
+def striptotitle(Xs):
+    for x in Xs:
+        x[3] = x[3].split(' ')[1]
+    return Xs
+
 def create_model(dataset):
     model = initialize_model()
-    model = train_model(model, dataset)
+    
+    model.fit(dataset)
     return model
 
 def initialize_model():
-    
+    """ initializes the model using sklearn objects """
+    # create objects
+    titleStripper = FunctionTransformer(striptotitle)
+
+    # neural net.  needs: 
+    #  number of hidden layers and number of neurons in each layer
+    estimator = MLPClassifier()
+
+    # create pipeline TODO: add objects to argument list
+    return make_pipeline(titleStripper, estimator)   
--- read_data.py
+++ read_data.py
@@ -1,10 +1,15 @@
 """ defines functions that read data from a csv file, 
     and return the data in a numpy dataset              """
 
-import pandas as pd
+import csv
 
 def read_data(filename):
     """ given a filename, open the file and read the information,
         format it, then return the data """
     print("Reading data from", filename)
-    return pd.read_csv(filename)
+    with open(filename) as f:
+        reader = csv.reader(f)
+        data = []
+        for row in reader:
+            data.append(row)
+        return data
